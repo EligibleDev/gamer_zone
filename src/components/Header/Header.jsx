dbb3578f141bac22637d0333fef47e27f98db23f
EligibleDev <eligibledev@gmail.com>
2023-10-07 18:27:45 +0600

    header done for all devices

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,22 @@
 import { NavLink } from "react-router-dom";
 import Logo from "./Logo/Logo";
-import { Button } from "@material-tailwind/react";
+import { Button, IconButton, Collapse  } from "@material-tailwind/react";
 import { FiLogIn } from "react-icons/fi";
+import { HiMenu } from "react-icons/hi";
 import ProfileMenu from "./ProfileMenu/ProfileMenu";
+import { useEffect, useState } from "react";
 
 const Header = () => {
+    const [isNavOpen, setIsNavOpen] = useState(false);
+    const toggleIsNavOpen = () => setIsNavOpen((cur) => !cur);
+
+    useEffect(() => {
+        window.addEventListener(
+            "resize",
+            () => window.innerWidth >= 960 && setIsNavOpen(false)
+        );
+    }, []);
+
     const navLinks = [
         {
             name: "Home",
@@ -22,7 +34,7 @@ const Header = () => {
 
     return (
         <header className="sticky top-0 left-0 right-0 z-50 border-b border-b-[rgba(255,255,255,.05)] bg-[var(--black)]">
-            <nav className="max-w-screen-xl mx-auto flex justify-between ">
+            <nav className="max-w-screen-xl mx-auto flex justify-between lg:items-center">
                 <nav className="lg:flex flex-1 gap-2 justify-center items-end lg:order-1 hidden">
                     {navLinks.map((link) => (
                         <NavLink
@@ -35,15 +47,35 @@ const Header = () => {
                     ))}
                 </nav>
 
-                <span className="lg:order-2 order-1 ">
+                <span className="lg:order-2 order-1">
                     <Logo />
                 </span>
 
-                <nav className="lg:hidden order-2">
-                        mobile browser
+                <nav className="lg:hidden order-3 flex-1 text-right">
+                    <IconButton
+                        size="sm"
+                        color="white"
+                        variant="text"
+                        onClick={toggleIsNavOpen}
+                        className="mt-4"
+                    >
+                        <HiMenu className="h-6 w-6" />
+                    </IconButton>
+
+                    <Collapse  open={isNavOpen} className="overflow-scroll">
+                        {navLinks.map((link) => (
+                            <NavLink
+                                className=" hover:border-[var(--red)] flex flex-col justify-center items-center transition duration-500 text-xs tracking-wider uppercase font-bold"
+                                key={link?.name}
+                                to={link?.path}
+                            >
+                                <span className="p-1">{link?.name}</span>
+                            </NavLink>
+                        ))}
+                    </Collapse >
                 </nav>
 
-                <nav className="flex-1 flex items-center justify-end order-3">
+                <nav className="flex-1 flex lg:items-center items-start justify-end order-3 mt-4 lg:mt-0">
                     <ProfileMenu />
 
                     <div className="text-right hidden">
